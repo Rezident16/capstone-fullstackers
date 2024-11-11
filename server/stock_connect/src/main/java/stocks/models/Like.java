15ba71ee07defe2fd6e9ed581305e7ba32846af5
sivaShilpa <shilpa.vchand@gmail.com>
2024-11-11 12:08:10 -0500

    Like Model complete

---
+++ server/stock_connect/src/main/java/stocks/models/Like.java
@@ -0,0 +1,40 @@
+package stocks.models;
+
+public class Like {
+    private int likeId;
+    private boolean isLiked;
+    private int userId;
+    private int messageId;
+
+    public int getLikeId() {
+        return likeId;
+    }
+
+    public void setLikeId(int likeId) {
+        this.likeId = likeId;
+    }
+
+    public boolean isLiked() {
+        return isLiked;
+    }
+
+    public void setLiked(boolean liked) {
+        isLiked = liked;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(int messageId) {
+        this.messageId = messageId;
+    }
+}
