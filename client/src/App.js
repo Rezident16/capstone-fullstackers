eb3170acaf7fedcf4ba50279bb78916a31286d78
sivaShilpa <shilpa.vchand@gmail.com>
2024-11-13 12:18:33 -0500

    Navbar frontend complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
-import logo from './logo.svg';
 import { BrowserRouter as Router } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 import Home from "./Home";
 import './App.css';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route path="/login" element={<SignIn/>} />
-      <Route path="/register" element={<SignUp/>} />
-      <Route path="/" element={<Home/>} />
-    </Routes>
-   </Router>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<SignUp />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
